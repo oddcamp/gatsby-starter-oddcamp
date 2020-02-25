516afe5f82395962443c58b4530d40111f8113c1
Osvaldas <osvaldas.v@gmail.com>
2020-02-25 12:37:53 +0200

    Fix CookieConsent layout for mobile

--- src/components/cookies-consent.js
+++ src/components/cookies-consent.js
@@ -5,20 +5,38 @@ import styled from "styled-components"
 import { rem } from "polished"
 
 const Container = styled.div`
-  max-width: 20em;
+  width: 100%;
   position: fixed;
   z-index: ${props => props.theme.ziCookiesConsent};
-  bottom: ${rem(30)};
-  right: ${rem(30)};
+  bottom: 0;
+  right: 0;
+  padding: ${rem(30)};
+  display: flex;
+  justify-content: flex-end;
+  pointer-events: none;
+
+  @media ${props => props.theme.xsmallDown} {
+    padding: ${rem(10)};
+  }
+`
+
+const Box = styled.div`
+  width: 100%;
+  max-width: 20em;
   padding: 1.4em;
+  pointer-events: auto;
   color: ${props => props.theme.colorWhite};
   background-color: ${props => props.theme.colorPurple};
+`
 
-  p {
-    margin-bottom: 1em;
-  }
+const Text = styled.p`
+  margin-bottom: 1em;
+`
 
-  button {
+const Cta = styled.div`
+  display: flex;
+
+  > button {
     padding: 0.5em 1em;
     height: 2.6em;
     border: 1px solid;
@@ -31,13 +49,10 @@ const Container = styled.div`
       opacity: 0.6;
     }
 
-    &:first-of-type {
+    &:first-child {
       margin-right: 0.5em;
       border-width: 2px;
       font-weight: ${props => props.theme.fwPrimaryBold};
-    }
-
-    &:last-of-type {
     }
   }
 `
@@ -67,26 +82,30 @@ const CookiesConsent = ({
 
   return (
     <Container {...props}>
-      <p
-        dangerouslySetInnerHTML={{
-          __html:
-            disclaimer ||
-            children ||
-            `Cookies will improve your browsing experiece. Would you like to accept the cookies?`,
-        }}
-      />
+      <Box>
+        <Text
+          dangerouslySetInnerHTML={{
+            __html:
+              disclaimer ||
+              children ||
+              `Cookies will improve your browsing experiece. Would you like to accept the cookies?`,
+          }}
+        />
 
-      <button
-        type="button"
-        onClick={() => acceptCookies(true)}
-        dangerouslySetInnerHTML={{ __html: ctaAccept || `Accept` }}
-      />
+        <Cta>
+          <button
+            type="button"
+            onClick={() => acceptCookies(true)}
+            dangerouslySetInnerHTML={{ __html: ctaAccept || `Accept` }}
+          />
 
-      <button
-        type="button"
-        onClick={() => acceptCookies(false)}
-        dangerouslySetInnerHTML={{ __html: ctaDeny || `Deny` }}
-      />
+          <button
+            type="button"
+            onClick={() => acceptCookies(false)}
+            dangerouslySetInnerHTML={{ __html: ctaDeny || `Deny` }}
+          />
+        </Cta>
+      </Box>
     </Container>
   )
 }
