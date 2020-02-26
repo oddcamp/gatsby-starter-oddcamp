41e017dc1364f304aaff66943cf183b1bd378c09
Osvaldas Valutis <osvaldas@kollegorna.se>
2020-02-26 15:45:12 +0200

    Fix partiallyActive for Link

--- src/components/link.js
+++ src/components/link.js
@@ -1,35 +1,41 @@
 import React from "react"
+import { Location } from "@reach/router"
 import PropTypes from "prop-types"
 import { Link as GatsbyLink } from "gatsby"
 import { startsWith } from "lodash"
+import filterInvalidDOMProps from "filter-invalid-dom-props"
 
-const Link = ({ children, to, ...rest }) => {
-  const isInternal = /^\/(?!\/)/.test(to)
+const Link = ({ children, to, internal, ...rest }) => {
+  if (typeof internal === `undefined`) {
+    internal = /^\/(?!\/)/.test(to)
+  }
 
-  if (isInternal) {
-    if (
-      rest.partiallyActive &&
-      typeof window !== `undefined` &&
-      window.location.pathname !== `/`
-    ) {
-      rest.partiallyActive = false
-    }
-
+  if (internal) {
     return (
-      <GatsbyLink to={to} {...rest}>
-        {children}
-      </GatsbyLink>
+      <Location>
+        {({ location }) => {
+          if (rest.partiallyActive && location.pathname !== `/`) {
+            rest.partiallyActive = false
+          }
+
+          return (
+            <GatsbyLink to={to} {...rest}>
+              {children}
+            </GatsbyLink>
+          )
+        }}
+      </Location>
     )
   }
 
   if (rest.target && !rest.rel) {
-    rest.rel = `noopener noreferrer`
+    rest.rel = `noopener`
   } else if (startsWith(to, `tel:`) && !rest.rel) {
     rest.rel = `nofollow`
   }
 
   return (
-    <a href={to} {...rest}>
+    <a href={to} {...filterInvalidDOMProps(rest)}>
       {children}
     </a>
   )
@@ -38,6 +44,7 @@ const Link = ({ children, to, ...rest }) => {
 Link.propTypes = {
   children: PropTypes.node,
   to: PropTypes.string.isRequired,
+  internal: PropTypes.bool,
 }
 
 export default Link
