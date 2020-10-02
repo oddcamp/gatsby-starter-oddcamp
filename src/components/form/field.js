d4fb7998999a80ab4d177cf4bd29562f5a5e978e
Osvaldas Valutis <osvaldas.v@gmail.com>
2020-10-02 11:25:06 +0300

    Wip

--- src/components/form/field.js
+++ src/components/form/field.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react"
 import PropTypes from "prop-types"
 import uuid from "uuid-random"
 import styled, { css } from "styled-components"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import React from "react"
 import PropTypes from "prop-types"
 import { graphql } from "gatsby"
 import styled from "styled-components"
